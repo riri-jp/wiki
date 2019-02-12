32f37f21cab1f8b61ef6d40fefd52e2fceedfe2a
riri-jp <alahmary.mashari@gmail.com>
2019-02-12 13:52:12 +0300

    Mashari&Fatima

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Page
 
-# Create your views here.
+
+def page_list_view(request):
+    context = {
+        "pages":Page.objects.all()
+    }
+    return render(request, 'list.html', context)
+
+
+
+
+def page_detail_view(request, page_id):
+    context = {
+        "page":Page.objects.get(id=page_id)
+    }
+    return render(request, 'detail.html', context)
+
